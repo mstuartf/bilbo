dc6885298bbbfa75968af54293d1c024a41e6b79
Michael Flood <michaelflood@Michaels-MacBook-Pro.local>
2019-03-31 17:04:24 +0100

    new bill dropdown

--- src/helpers/mat.module.ts
+++ src/helpers/mat.module.ts
@@ -3,10 +3,12 @@ import { MatInputModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -22,7 +24,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   	MatDialogModule,
   	MatTableModule,
   	MatIconModule,
-  	MatSortModule
+  	MatSortModule,
+    MatSelectModule
   ],
   exports: [
   	BrowserAnimationsModule, 
@@ -34,7 +37,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   	MatDialogModule,
   	MatTableModule,
   	MatIconModule,
-  	MatSortModule
+  	MatSortModule,
+    MatSelectModule
   ]
 })
 export class MatModule { }
